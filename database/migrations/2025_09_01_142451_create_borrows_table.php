58ea2475f6cdbd580335e82348d81f97a685f2cc
seham mohsen <sehammohsen84@gmail.com>
2025-09-01 19:56:01 +0300

    Databases columns commit

--- database/migrations/2025_09_01_142451_create_borrows_table.php
+++ database/migrations/2025_09_01_142451_create_borrows_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('borrows', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('book_id')->constrained('books')->onDelete('cascade');
+            $table->dateTime('borrowed_at');
+            $table->dateTime('due_date');
+            $table->dateTime('returned_at')->nullable();
+            $table->enum('status', ['borrowed', 'returned', 'overdue'])->default('borrowed');
             $table->timestamps();
         });
     }
