7a6bf01c154f487e60b626344e854d7d6354428b
SehamMohsen <sehammohsen84@gmail.com>
2025-09-04 14:34:41 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,57 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Student\BookController;
-use App\Http\Controllers\Student\ProfileController;
+
+use App\Http\Controllers\Admin\BookController as AdminBookController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+use App\Http\Controllers\Admin\ProfileController as AdminProfileController;
+
+use App\Http\Controllers\Student\BookController as StudentBookController;
+use App\Http\Controllers\Student\ProfileController as StudentProfileController;
 use App\Http\Controllers\BorrowController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Books
-Route::get('/books', [BookController::class, 'index'])->name('books.index');
-Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Auth Required
 Route::middleware('auth')->group(function () {
-    // Dashboard + Profile
-    Route::get('/dashboard', [ProfileController::class, 'dashboard'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile/delete', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
+Route::middleware(['auth'])->group(function () {
+    Route::middleware('isAdmin')->prefix('admin')->name('admin.')->group(function () {
+        Route::get('dashboard', [AdminBookController::class, 'dashboard'])->name('dashboard');
+        Route::resource('books', AdminBookController::class);
+        Route::resource('users', AdminUserController::class)->only(['index','show']);
+        Route::get('profile', [AdminProfileController::class, 'edit'])->name('profile.edit');
+        Route::put('profile', [AdminProfileController::class, 'update'])->name('profile.update');
+        Route::get('students/search', [AdminUserController::class, 'search'])->name('students.search');
+    });
+});
 
+Route::middleware(['auth', 'isStudent'])->prefix('student')->name('student.')->group(function () {
+    // Dashboard + Profile
+    Route::get('dashboard', [StudentProfileController::class, 'dashboard'])->name('dashboard');
+    Route::get('profile', [StudentProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('profile/update', [StudentProfileController::class, 'update'])->name('profile.update');
+    Route::delete('profile/delete', [StudentProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Books
+    Route::get('books', [StudentBookController::class, 'index'])->name('books.index');
+    Route::get('books/{id}', [StudentBookController::class, 'show'])->name('books.show');
 
     // Borrow
-    Route::get('/my-borrows', [BorrowController::class, 'index'])->name('borrows.index');
-    Route::post('/borrow/{book_id}', [BorrowController::class, 'borrow'])->name('borrow.book');
-    Route::post('/return/{book_id}', [BorrowController::class, 'returnBook'])->name('return.book');
-    Route::get('/borrowed-books', [BorrowController::class, 'allBorrowedBooks'])
-    ->name('borrows.all');
-
+    Route::get('my-borrows', [BorrowController::class, 'index'])->name('borrows.index');
+    Route::post('borrow/{book_id}', [BorrowController::class, 'borrow'])->name('borrow.book');
+    Route::post('return/{book_id}', [BorrowController::class, 'returnBook'])->name('return.book');
+    Route::get('borrowed-books', [BorrowController::class, 'allBorrowedBooks'])->name('borrows.all');
 });
 
 require __DIR__.'/auth.php';
